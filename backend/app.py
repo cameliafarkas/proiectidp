c8bb4445184d9fe9d9eeed5f4e72a8e29325768a
cameliafarkas <camelia.farkas@gmail.com>
2020-03-08 14:43:55 +0200

    Add files via upload

---
+++ backend/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+from flask import request
+import random
+import string
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    my_string = "Professional Hair Shop"
+    return render_template("index.html", body=my_string)
+
+@app.route('/products')
+def products():
+    return render_template("products.html")
+
+@app.route('/products/type/<type>')
+def get_products(type):
+    products = []
+
+    return render_template("products.html", products=products)
+
+@app.route('/shopping-cart')
+def shop():
+    my_string = "Shopping cart"
+    return render_template("shopping_cart.html", body=my_string)
+
+@app.route('/payment')
+def pay():
+    my_string = "Pay"
+    return render_template("payment.html", body=my_string)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
